9d2255367ac7d12c553d638b638013bf855186ab
ddssamu3l <72890797+ddssamu3l@users.noreply.github.com>
2024-07-03 18:16:06 -0700

    convex quickstart completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import { Button } from "@/components/ui/button";
+"use client";
 import { podcastData } from '@/data';
 import PodcastCard from "@/components/ui/PodcastCard";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
 
 const Home = () => {
+  const tasks = useQuery(api.tasks.get);
   return (
     <div className = "mt-9 flex flex-col gap-9">
       <section className = "flex flex-col gap-5">
         <h1 className = "text-20 font-bold text-white-1">Trending Podcasts</h1>
+        <div className="flex min-h-screen flex-col items-center justify-between p-24 text-white-1">
+          {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+        </div>
         <div className = "podcast_grid">
           {podcastData.map(({id, title, description, imgURL}) => (
             <PodcastCard 
