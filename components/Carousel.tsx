7f462340ba44decc66cf418041f13cba9f00931b
ddssamu3l <72890797+ddssamu3l@users.noreply.github.com>
2024-07-13 12:51:55 -0700

    RightSidebar Header and Carousel components created

---
+++ components/Carousel.tsx
@@ -0,0 +1,62 @@
+import React, { useCallback } from 'react'
+import { EmblaOptionsType, EmblaCarouselType } from 'embla-carousel'
+import { DotButton, useDotButton } from './EmblaCarouselDotButton'
+import Autoplay from 'embla-carousel-autoplay'
+import useEmblaCarousel from 'embla-carousel-react'
+import { CarouselProps } from '@/types'
+import { useRouter } from 'next/navigation'
+
+const EmblaCarousel = ({fansLikeDetail}: CarouselProps) => {
+  const router = useRouter();
+
+  const [emblaRef, emblaApi] = useEmblaCarousel({loop: true}, [Autoplay()])
+
+  const onNavButtonClick = useCallback((emblaApi: EmblaCarouselType) => {
+    const autoplay = emblaApi?.plugins()?.autoplay
+    if (!autoplay || !("stopOnInteraction" in autoplay.options)) return
+
+    const resetOrStop =
+      autoplay.options.stopOnInteraction === false
+        ? (autoplay.reset as () => void)
+        : (autoplay.stop as () => void)
+
+    resetOrStop()
+  }, [])
+
+  const { selectedIndex, scrollSnaps, onDotButtonClick } = useDotButton(
+    emblaApi,
+    onNavButtonClick
+  )
+
+  const slides = fansLikeDetail && fansLikeDetail?.filter((item: any) => item.totalPodcasts > 0)
+
+  return (
+    <section className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {slides.map((index) => (
+            <div className="embla__slide" key={index}>
+              <div className="embla__slide__number">{index + 1}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="embla__controls">
+        <div className="embla__dots">
+          {scrollSnaps.map((_, index) => (
+            <DotButton
+              key={index}
+              onClick={() => onDotButtonClick(index)}
+              className={'embla__dot'.concat(
+                index === selectedIndex ? ' embla__dot--selected' : ''
+              )}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default EmblaCarousel
--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,14 +1,37 @@
 'use client'
-import { SignedIn, useUser } from '@clerk/nextjs'
+import { SignedIn, UserButton, useUser } from '@clerk/nextjs'
+import Link from 'next/link';
+import Image from 'next/image'
 import React from 'react'
+import Header from './Header';
+import Carousel from './carousel';
+import { api } from '@/convex/_generated/api';
+import { useQuery } from 'convex/react';
 
 const RightSidebar = () => {
   const {user} = useUser();
+  const topPodcasters = useQuery(api.users.getTopUserByPodcastCount)
   return (
     <section className = "right_sidebar text-white-1">
       <SignedIn>
-        <h1>{user?.firstName}</h1>
+        <Link href = {`/profile/${user?.id}`} className = "flex gap-3 pb-12">
+          <UserButton />
+          <div className = "flex w-full items-center justify-between">
+            <h1 className = "text-16 truncate font-semibold text-white-1">{user?.firstName} {user?.lastName}</h1>
+            <Image 
+              src = "/icons/right-arrow.svg"
+              alt="arrow"
+              width={24}
+              height={24}
+            />
+          </div>
+        </Link>
       </SignedIn>
+
+      <section>
+        <Header headerTitle = "Fans Like You"/>
+        {/* <Carousel fansLikeDetail={topPodcasters!}/> */}
+      </section>
     </section>
   )
 }
