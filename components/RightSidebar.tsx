5642a1f9de5ce84e14eaafb95969dc3e370fe79a
ddssamu3l <72890797+ddssamu3l@users.noreply.github.com>
2024-07-13 20:33:12 -0700

    RightSidebar fully completed

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -7,10 +7,14 @@ import Header from './Header';
 import Carousel from './Carousel';
 import { api } from '@/convex/_generated/api';
 import { useQuery } from 'convex/react';
+import { useRouter } from 'next/navigation';
+import LoaderSpinner from './LoadSpinner';
 
 const RightSidebar = () => {
   const {user} = useUser();
   const topPodcasters = useQuery(api.users.getTopUserByPodcastCount);
+  const router = useRouter();
+  if(!topPodcasters) return <LoaderSpinner />
   return (
     <section className = "right_sidebar text-white-1">
       <SignedIn>
@@ -29,8 +33,38 @@ const RightSidebar = () => {
       </SignedIn>
 
       <section>
-        <Header headerTitle = "Popular Podcasters"/>
+        <Header headerTitle = "See Active Podcasters"/>
         <Carousel fansLikeDetail={topPodcasters!}/>
+      </section>
+
+      <section className = "flex flex-col gap-8 pt-12">
+        <Header headerTitle = "Top Podcasters"/>
+        {topPodcasters?.slice(0, 4).map((podcaster) => (
+          <div key={podcaster._id} className = "flex cursor-pointer justify-between" onClick={() => router.push(`/profile/${podcaster.clerkId}`)}>
+            <figure className = "flex items-center gap-2">
+              <Image 
+                src={podcaster.imageURL}
+                alt={podcaster.username}
+                width={44}
+                height={44}
+                className = "aspect-square rounded-lg"
+              />
+              <h2 className = "text-14 font-semibold text-white-1">{podcaster.username}</h2>
+            </figure>
+            <div className = "flex items-center">
+              
+              {podcaster.totalPodcasts === 1 ? (
+                <p className = "text-12 font-normal">
+                 {podcaster.totalPodcasts} podcast
+                </p>
+              ):(
+                <p className = "text-12 font-normal">
+                {podcaster.totalPodcasts} podcasts
+               </p>
+              )}
+            </div>
+          </div>
+        ))}
       </section>
     </section>
   )
