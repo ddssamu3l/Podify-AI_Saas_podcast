6a89f550cf4867cda464198afb2fef46949fd124
ddssamu3l <72890797+ddssamu3l@users.noreply.github.com>
2024-07-14 21:34:12 -0700

    MobileNav completed

--- app/(root)/discover/page.tsx
+++ app/(root)/discover/page.tsx
@@ -5,6 +5,7 @@ import LoaderSpinner from '@/components/LoadSpinner'
 import PodcastCard from '@/components/PodcastCard'
 import SearchBar from '@/components/SearchBar'
 import { api } from '@/convex/_generated/api'
+import { cn } from '@/lib/utils'
 import { useQuery } from 'convex/react'
 import React from 'react'
 
@@ -14,7 +15,7 @@ const Discover = ({searchParams: {search}}: {searchParams: {search: string}}) =>
     <div className="flex flex-col gap-9">
       <SearchBar />
       <div className = "flex flex-col gap-9">
-        <h1 className = "text-20 font-bold text-white-1">Discover Podcasts</h1>
+        <h1 className = "text-20 font-bold text-white-1">{!search ? 'Discover Podcasts' : `Searching For: ${search}`}</h1>
 
         {podcastsData ? (
           <>
--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,8 +1,62 @@
+'use client'
 import React from 'react'
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import Image from 'next/image'
+import Link from 'next/link'
+import { sidebarLinks } from '@/data'
+import { usePathname } from 'next/navigation'
+import { cn } from '@/lib/utils'
+
 
 const MobileNav = () => {
+  const pathname = usePathname();
   return (
-    <div>Mobilenav</div>
+    <section>
+      <Sheet>
+        <SheetTrigger>
+          <Image 
+            src = "/icons/hamburger.svg"
+            alt="menu"
+            width={30}
+            height={30}
+            className = "mt-4 cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent side="left" className = "border-none bg-black-0 w-[280px]">
+
+          <Link href= "/" className = "flex cursor-pointer items-center gap-1 pb-10 pl-4">
+            <Image src = "/logo.svg" alt = "logo" width = {23} height = {27}/>
+            <h1 className = "text-24 font-extrabold text-white-1">Podify</h1>
+          </Link>
+
+          <div className = "flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
+            <SheetClose asChild>
+              <nav>
+              {sidebarLinks.map(({route, label, imgURL, size}) => {
+            // isActive triggers when the current page path's name matches a "route" from data/index.ts.
+            // if isActive is active, then we apply a shaadow gradient border on the current sidebar link to show which page we are currently at
+              const isActive = pathname === route;
+              // cn stans for "classname". Its a tailwind function that allows you to pass multiple classnames
+              return <SheetClose asChild key={route}><Link href={route} key={label} className={cn("flex gap-3 items-center py-4 max-lg:px-4 font-manrope text-white-1 font-semibold justify-start", {
+                'bg-nav-focus border-r-4 border-yellow': isActive
+              })}>
+                  <Image src = {imgURL} alt={label} width={size} height={size}/>
+                  <p>{label}</p>
+                </Link>
+              </SheetClose>
+          })}
+              </nav>
+            </SheetClose>
+          </div>
+        </SheetContent>
+      </Sheet>
+
+    </section>
   )
 }
 
